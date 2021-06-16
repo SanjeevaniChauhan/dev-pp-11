529f81f496747704fcd7144ffcad581a65a12db7
Sanjeevani chauhan <sanchauhan00001@gmail.com>
2021-06-16 20:31:41 +0530

    My Commit

---
+++ module1/lec1/activity/fs.js
@@ -0,0 +1,5 @@
+const fs=require("fs");
+let f1Data=fs.readFileSync("./f1.txt");
+console.log(f1Data+"");
+fs.writeFileSync("index.html","Hello world");
+fs.writeFileSync("../activity/activity.js","fgzjhggghz");
